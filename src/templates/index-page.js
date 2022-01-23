3e8072948f3902ad478b2dd41ffe1628b87bb42c
Mark Fazzio <mark@pixelpandas.com>
2022-01-23 12:49:52 -0500

    remove annoying autoplay

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -29,7 +29,6 @@ export const IndexPageTemplate = ({
           height="300"
           scrolling="no"
           frameborder="no"
-          allow="autoplay"
           src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1202178046&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
         />
         <div className="site-title-wrapper has-text-centered">
