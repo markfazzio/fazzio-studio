2c32c16fad626dcc2405368cf0bfe66e18b59b0c
Mark Fazzio <mark@pixelpandas.com>
2022-01-23 19:33:39 -0500

    update meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "Fazzio Studio",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "Welcome to Fazzio Studio, personal website of Mark Fazzio, JAMStack engineer, musician & car enthusiast. Creator of the Digital Pokemon TCG project.",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
