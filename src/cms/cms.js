d61a9fe3aa0a0dedc2613b8d5d882776aa499120
Mark Fazzio <mark@pixelpandas.com>
2022-01-25 23:32:55 -0500

    remove junk text editor, will come back to this

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -6,9 +6,6 @@ import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
 import CodePagePreview from "./preview-templates/CodePagePreview";
 import IndexPagePreview from "./preview-templates/IndexPagePreview";
-import RichTextEditorPreview from "./preview-templates/RichTextEditorPreview";
-
-import RichTextEditor from "../components/RichTextEditor";
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
@@ -17,5 +14,3 @@ CMS.registerPreviewTemplate("index", IndexPagePreview);
 CMS.registerPreviewTemplate("about", AboutPagePreview);
 CMS.registerPreviewTemplate("ts-algorithms", CodePagePreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
-
-CMS.registerWidget("richtext", RichTextEditor, RichTextEditorPreview);
--- src/cms/preview-templates/RichTextEditorPreview.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const RichTextEditorPreview = (props) => {
-  return <div dangerouslySetInnerHTML={{ __html: props.value }} />;
-};
-
-export default RichTextEditorPreview;
